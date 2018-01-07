988ea8bd245c9354a187601f1b8f80d42dda5572
mmiscool <mike@smbisoft.com>
2018-01-07 15:34:48 -0500

    More updates to the engine. Added properties UI eliment.

--- engine.js
+++ engine.js
@@ -1,13 +1,15 @@
 var MaxSymbols = 100;
 var MaxLayout = 50;
 var MaxConnections = 50;
-
+var CurrentToolStatus = "symbolPic";
 
 function CircuitSymbols() {
     this.delete = function () {
         this.Name = "";
         this.symbolPoints = "";
         this.padPoints = "";
+        this.width = 0;
+        this.height = 0;
         return "deleted";
     };
 
@@ -43,6 +45,33 @@ function CircuitLayout() {
         this.SchematicX = x;
         this.SchematicY = y;
     };
+
+    this.DetectIfSymbolUnderXY = function (x, y) {
+        if (this.SymbolID <= 0) return false;
+
+        wid = Symbols[this.SymbolID].width;
+        hei = Symbols[this.SymbolID].height;
+
+        if (this.SchematicRotation == 90 || this.SchematicRotation == 90) [wid, hei] = [hei, wid];
+
+        if (x > this.SchematicX - wid / 2 && x < this.SchematicX + wid / 2 && y > this.SchematicY - hei / 2 && y < this.SchematicY + hei / 2) {
+
+            return 1;
+
+        }
+        return 0;
+    };
+
+
+    this.UIshowProperties = function () {
+
+        document.getElementById("SymbolID").value = this.SymbolID;
+        document.getElementById("SchematicX").value = this.SchematicX;
+        document.getElementById("SchematicY").value = this.SchematicY;
+        document.getElementById("SchematicRotation").value = this.SchematicRotation;
+
+    };
+
     this.delete();
 }
 
@@ -89,27 +118,92 @@ var context = canvas.getContext('2d');
 canvas.addEventListener('click', function (evt) {
     var mousePos = getMousePos(canvas, evt);
 
+    if (CurrentToolStatus == "symbolPic") {
+        UIselectedSymbolID = CheckLayoutSymbolClick(mousePos.x, mousePos.y);
+        UIshowSymbolLayoutInfo(UIselectedSymbolID);
+        //CheckLayoutSymbolClick(mousePos.x, mousePos.y);
+    }
 
 
-    Layout[1].moveSymbol(mousePos.x, mousePos.y);
-
+    if (CurrentToolStatus == "moveSymbol") {
+        UIselectedSymbolID = document.getElementById("LayoutID").value;
+        Layout[UIselectedSymbolID].moveSymbol(mousePos.x, mousePos.y);
+        UIshowSymbolLayoutInfo(UIselectedSymbolID);
+        CurrentToolStatus = "symbolPic"
+    }
 
     renderLayout();
-
 }, false);
+
+
+function UIshowSymbolLayoutInfo(mousePos) {
+
+
+    if (UIselectedSymbolID) {
+        document.getElementById("LayoutID").value = UIselectedSymbolID;
+        Layout[UIselectedSymbolID].UIshowProperties();
+    }
+    else {
+        document.getElementById("LayoutID").value = UIselectedSymbolID;
+        document.getElementById("SymbolID").value = "";
+        document.getElementById("SchematicX").value = "";
+        document.getElementById("SchematicY").value = "";
+        document.getElementById("SchematicRotation").value = "";
+    }
+
+}
+
+function UIsymbolLayoutButtonClick(ActionToBeTaken) {
+    UIselectedSymbolID = document.getElementById("LayoutID").value;
+    if (UIselectedSymbolID === "undefined" || UIselectedSymbolID === "" || UIselectedSymbolID === null) return;
+
+
+    if (ActionToBeTaken == "delete") {
+        Layout[UIselectedSymbolID].delete()
+        renderLayout();
+        UIshowSymbolLayoutInfo(UIselectedSymbolID);
+    }
+
+
+    if (ActionToBeTaken == "move") {
+        CurrentToolStatus = "moveSymbol";
+    }
+}
+
+
+function CheckLayoutSymbolClick(x, y) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    for (i = 1; i <= MaxLayout; i++) {
+        if (Layout[i].DetectIfSymbolUnderXY(x, y)) {
+            return i;
+        }
+    }
+}
+
+
+Symbols[1].Name = "test";
+Symbols[1].width = 74;
+Symbols[1].height = 100;
+
 
 Layout[1].SymbolID = 1;
 
 
 Layout[5].SymbolID = 1;
-Layout[5].SchematicX = 50;
+Layout[5].SchematicX = 400;
+Layout[5].SchematicX = 400;
+
+Layout[6].SymbolID = 1;
+Layout[6].SchematicX = 50;
+Layout[6].SchematicRotation = 90;
+Layout[6].SchematicY = 350;
 
 
 function RenderLayoutItem(id) {
     if (Layout[id].SymbolID <= 0) return;
     var img = new Image();
     img.src = './symbols/' + Layout[id].SymbolID + '.png';
-    drawRotatedImage(img, Layout[id].SchematicX, Layout[id].SchematicY, 90);
+    drawRotatedImage(img, Layout[id].SchematicX, Layout[id].SchematicY, Layout[id].SchematicRotation);
 }
 
 
