7b964988446e504dd0dc0568df263ce021dd8478
mmiscool <mike@smbisoft.com>
2018-01-07 17:18:30 -0500

    Added pin listing loading to the Symbol editor

--- SymbolEditor.js
+++ SymbolEditor.js
@@ -6,22 +6,54 @@ function UIsymbolButtonClick(ActionToBeTaken) {
 
 
 function UIloadItem() {
-    alert(new URL(window.location.href).searchParams.get("id"));
+    SymbolID = new URL(window.location.href).searchParams.get("id");
+
+
+    var img = new Image();
+    img.src = './symbols/' + SymbolID + '.png';
+    img.onload = function () {
+        context.drawImage(img, 0, 0);
+    }
+
+
+    document.getElementById("PinListing").innerHTML = "";
 
 
     var lines = document.getElementById('TextPinListing').innerHTML.split('\n');
     for (var i = 0; i < lines.length; i++) {
-        alert(lines[i]);
+        if (lines[i] != "") UIaddItemToSelect("PinListing", lines[i]);
     }
 }
 
 
 
 
+function UIaddItemToSelect(id, optionToAdd) {
+    var option = document.createElement("option");
+    option.text = optionToAdd;
+    document.getElementById(id).add(option);
+}
 
 
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
+
+var canvas = document.getElementById('myCanvas');
+var context = canvas.getContext('2d');
+
+canvas.addEventListener('click', function (evt) {
+    var mousePos = getMousePos(canvas, evt);
 
 
+    alert(mousePos.x + " " + mousePos.y);
 
+
+    renderLayout();
+}, false);
 
 
